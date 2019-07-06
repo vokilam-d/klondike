66800bd3783b771055c93f41e85a08405f5b5f83
Malikov Denis <ryki.admirala@gmail.com>
2019-07-06 23:29:54 +0300

    add cache to dynamic module resolver

--- packages/web/src/app/functions/dynamic-module-resolver.function.ts
+++ packages/web/src/app/functions/dynamic-module-resolver.function.ts
@@ -1,19 +1,31 @@
 import axios from 'axios';
 
+const MODULES_CACHE: { [slug: string]: any; } = { };
+
 export function dynamicModuleResolver(slug: string = 'not-found') {
+  if (MODULES_CACHE[slug]) {
+    return () => MODULES_CACHE[slug];
+  }
+
   return () => axios.get(`http://localhost:3500/api/pages/${slug}`)
       .then(res => {
         const pageType = res.data;
-
         return import(`../pages/${pageType}/${pageType}.module`)
-          .then(module => {
-            const keys = Object.keys(module);
-            return module[keys[0]];
+          .then(importedModule => {
+            const keys = Object.keys(importedModule);
+            const ngModule =  importedModule[keys[0]];
+
+            MODULES_CACHE[slug] = ngModule;
+            return ngModule;
           })
           .catch(() => {
             console.warn(`Lazy load of module '${pageType}' failed. Loading Not found...`);
-            return import('../pages/not-found/not-found.module').then(m => m.NotFoundModule);
+            return importNotFound();
           });
       })
-      .catch(() => import('../pages/not-found/not-found.module').then(m => m.NotFoundModule))
+      .catch(() => importNotFound())
+}
+
+function importNotFound() {
+  return import('../pages/not-found/not-found.module').then(m => m.NotFoundModule);
 }
