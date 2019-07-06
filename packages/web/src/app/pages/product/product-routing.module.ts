f15bda8cd9796bd97e5855aa758c48d9c4ffbf06
Malikov Denis <ryki.admirala@gmail.com>
2019-07-06 23:17:47 +0300

    add Product page

--- packages/web/src/app/pages/product/product-routing.module.ts
+++ packages/web/src/app/pages/product/product-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './product.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ProductComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
