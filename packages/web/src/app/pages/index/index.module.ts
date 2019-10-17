15364d4bd22833a023b3c5932f2985320e175415
Malikov Denis <ryki.admirala@gmail.com>
2019-10-17 22:04:03 +0300

    add footer

--- packages/web/src/app/pages/index/index.module.ts
+++ packages/web/src/app/pages/index/index.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { IndexRoutingModule } from './index-routing.module';
 import { IndexComponent } from './index.component';
 import { HeaderModule } from '../../header/header.module';
+import { FooterModule } from '../../footer/footer.module';
 
 @NgModule({
   declarations: [IndexComponent],
   imports: [
     CommonModule,
     IndexRoutingModule,
-    HeaderModule
+    HeaderModule,
+    FooterModule
   ]
 })
 export class IndexModule { }
