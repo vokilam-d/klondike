7b0c862f8dea3b4996e19671f055cc46a433b59a
Malikov Denis <ryki.admirala@gmail.com>
2019-10-26 18:33:10 +0300

    prefix web-admin classes

--- packages/web-admin/src/app/app-routing.module.ts
+++ packages/web-admin/src/app/app-routing.module.ts
@@ -13,13 +13,13 @@ const routes: Routes = [
     loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.WebAdminDashboardModule)
   }, {
     path: 'order',
-    loadChildren: () => import('./pages/orders/orders.module').then(m => m.OrdersModule)
+    loadChildren: () => import('./pages/orders/orders.module').then(m => m.WebAdminOrdersModule)
   },
   {
     path: 'product',
-    loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule)
+    loadChildren: () => import('./pages/products/products.module').then(m => m.WebAdminProductsModule)
   },
-  { path: 'category', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) }
+  { path: 'category', loadChildren: () => import('./pages/categories/categories.module').then(m => m.WebAdminCategoriesModule) }
 ];
 
 @NgModule({
