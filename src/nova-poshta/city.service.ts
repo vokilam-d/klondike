72fb8d9442b92a936959e8c87ba5454557496e02
Yurii Malikov <malikov.yurii.a@gmail.com>
2020-04-16 01:47:21 +0300

    Code cleanup

--- src/nova-poshta/city.service.ts
+++ src/nova-poshta/city.service.ts
@@ -6,6 +6,8 @@ import { autocompleteSettings } from '../shared/constants';
 import { ClientSPFDto } from '../shared/dtos/client/spf.dto';
 import { IFilter } from '../shared/dtos/shared-dtos/spf.dto';
 import { PrimaryInstanceCron } from '../shared/decorators/primary-instance-cron.decorator';
+import { CityDto } from './models/city.dto';
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class CityService implements OnApplicationBootstrap {
@@ -19,6 +21,13 @@ export class CityService implements OnApplicationBootstrap {
     this.searchService.ensureCollection(ElasticCity.collectionName, new ElasticCity(), autocompleteSettings);
   }
 
+  public async getCities(spf: ClientSPFDto) : Promise<CityDto[]> {
+    const filters: IFilter[] = [{ fieldName: 'name|ruName', value: spf['filter'] }];
+    const searchResponse = await this.searchService.searchByFilters(ElasticCity.collectionName, filters, 0, spf.limit);
+    return plainToClass(CityDto, searchResponse[0],{ excludeExtraneousValues: true })
+      .sort((a, b) => a.name.localeCompare(b.name));
+  }
+
   @PrimaryInstanceCron(CronExpression.EVERY_DAY_AT_3AM)
   async loadCitiesToElastic() {
     let cityCount = 0;
@@ -28,7 +37,7 @@ export class CityService implements OnApplicationBootstrap {
       do {
         pageNumber++;
         cities = await this.fetchCityCatalogPage(pageNumber);
-        const esCities = cities.map(CityService.toCityDto);
+        const esCities = cities.map(c => CityService.toCityDto(c));
         this.searchService.addDocuments(ElasticCity.collectionName, esCities);
         cityCount += cities.length;
       } while (cities.length !== 0);
@@ -41,42 +50,46 @@ export class CityService implements OnApplicationBootstrap {
     this.logger.log(`Sent ${cityCount} cities to index`);
   }
 
-  private async fetchCityCatalogPage(cityBulkNumber: number) {
+  private async fetchCityCatalogPage(cityBulkNumber: number) : Promise<any[]> {
     let response = await this.http.post('http://api.novaposhta.ua/v2.0/json/Address/searchSettlements/',
       {
-        'modelName': 'AddressGeneral',
-        'calledMethod': 'getSettlements',
-        'methodProperties': {
-          'Page': cityBulkNumber,
-          'Warehouse': '1'
+        modelName: 'AddressGeneral',
+        calledMethod: 'getSettlements',
+        methodProperties: {
+          Page: cityBulkNumber,
+          Warehouse: '1'
         },
-        'apiKey': 'fc458ea324bd4fea1f1013dba44cdd03'
+        apiKey: process.env.NOVA_POSHTA_API_KEY
       }).toPromise();
 
     return response.data.data;
   }
 
-  private static toCityDto(city) {
-    const cityPrefix = city.SettlementTypeDescription
-      .replace(/селище міського типу/, 'смт.')
-      .replace(/село/, 'с.')
-      .replace(/селище/, 'с.')
-      .replace(/місто/, 'м.');
-    const regionsDescription = city.RegionsDescription ? ', ' + city.RegionsDescription : '';
+  private static toCityDto(city): CityDto {
+    const shortCityType = this.shortenCityType(city.SettlementTypeDescription);
+    let fullName = `${shortCityType} ${city.Description} (${city.AreaDescription}`;
+    if (city.RegionsDescription) {
+      fullName += ', ' + city.RegionsDescription
+    }
+    fullName += ')';
+
     return {
-      'id': city.Ref,
-      'name': city.Description,
-      'ruName': city.DescriptionRu,
-      'fullName': `${cityPrefix} ${city.Description} (${city.AreaDescription}${regionsDescription})`
+      id: city.Ref,
+      name: city.Description,
+      ruName: city.DescriptionRu,
+      fullName: fullName
     };
   }
 
-  async getCities(spf: ClientSPFDto) {
-    const filters: IFilter[] = [{ fieldName: 'name|ruName', value: spf['filter'] }];
-    const searchResponse = await this.searchService.searchByFilters(ElasticCity.collectionName, filters, 0, spf.limit);
-    return searchResponse[0]
-      .map(esCity => ({ id: esCity.id, name: esCity.fullName }))
-      .sort((a, b) => a.name.localeCompare(b.name));
+  private static shortenCityType(cityType: string): string {
+    if (cityType.includes('селище міського типу')) {
+      return 'смт.'
+    } else if (cityType.includes('село') || cityType.includes('селище')) {
+      return 'с.'
+    } else if (cityType.includes('місто')) {
+      return 'м.'
+    }
+    return '';
   }
 
 }
--- src/nova-poshta/models/city.dto.ts
+++ src/nova-poshta/models/city.dto.ts
@@ -1,6 +1,18 @@
+import { Expose } from 'class-transformer';
+import { IsString } from 'class-validator';
+
 export class CityDto {
 
+  @Expose()
+  @IsString()
   id: string;
+
+  @Expose()
+  @IsString()
+  fullName: string;
+
   name: string;
+
+  ruName: string;
 
 }
