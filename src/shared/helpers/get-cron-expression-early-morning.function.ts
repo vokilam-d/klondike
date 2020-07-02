e821f20e800afe46a252de0087f46c92c2c94a34
Malikov Denis <ryki.admirala@gmail.com>
2020-07-03 00:28:00 +0300

    fix getCronExpressionEarlyMorning

--- src/shared/helpers/get-cron-expression-early-morning.function.ts
+++ src/shared/helpers/get-cron-expression-early-morning.function.ts
@@ -1,11 +1,12 @@
 const STARTING_HOUR = 4;
 const STARTING_MINUTE = 10;
+const MINUTES_IN_HOUR = 60;
 
 let jobsCount = 0;
 
 export function getCronExpressionEarlyMorning() {
-  const minute = STARTING_MINUTE + (jobsCount * 3);
-  const hour = STARTING_HOUR + Math.floor(minute / 60);
+  const minute = (STARTING_MINUTE + (jobsCount * 3)) % MINUTES_IN_HOUR;
+  const hour = STARTING_HOUR + Math.floor(minute / MINUTES_IN_HOUR);
 
   jobsCount++;
 
