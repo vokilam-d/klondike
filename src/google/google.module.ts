a53c383f23893e86fe11edb880479aaabef072d7
Malikov Denis <ryki.admirala@gmail.com>
2020-07-18 13:10:23 +0300

    fix imports

--- src/google/google.module.ts
+++ src/google/google.module.ts
@@ -3,9 +3,10 @@ import { GoogleShoppingFeedService } from './google-shopping-feed.service';
 import { FeedController } from './feed.controller';
 import { ProductModule } from '../product/product.module';
 import { ProductReviewModule } from '../reviews/product-review/product-review.module';
+import { AttributeModule } from '../attribute/attribute.module';
 
 @Module({
-  imports: [ProductModule, ProductReviewModule],
+  imports: [ProductModule, ProductReviewModule, AttributeModule],
   providers: [GoogleShoppingFeedService],
   controllers: [FeedController]
 })
