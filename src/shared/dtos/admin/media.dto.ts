7ee0b3500cc13f825ad8e49c868f6ae73f9a21e0
Malikov Denis <ryki.admirala@gmail.com>
2020-07-22 18:57:32 +0300

    [KL-233] fixes for optional fields in MediaDto

--- src/shared/dtos/admin/media.dto.ts
+++ src/shared/dtos/admin/media.dto.ts
@@ -1,17 +1,20 @@
-import { IsBoolean, IsString } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 import { Expose } from 'class-transformer';
 import { MediaDto } from '../shared-dtos/media.dto';
 
 export class AdminMediaDto extends MediaDto {
   @Expose()
+  @IsOptional()
   @IsBoolean()
   isHidden?: boolean;
 
   @Expose()
+  @IsOptional()
   @IsString()
   size?: string;
 
   @Expose()
+  @IsOptional()
   @IsString()
   dimensions?: string;
 }
