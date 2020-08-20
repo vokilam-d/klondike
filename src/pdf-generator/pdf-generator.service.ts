b4259e15a91f901e19adfea9039e386613b85396
Malikov Denis <ryki.admirala@gmail.com>
2020-08-20 20:12:07 +0300

    fix zombie process leak - start single instance of browser

--- src/pdf-generator/pdf-generator.service.ts
+++ src/pdf-generator/pdf-generator.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap, OnApplicationShutdown } from '@nestjs/common';
 import * as fs from 'fs';
 import * as handlebars from 'handlebars';
 import * as puppeteer from 'puppeteer';
@@ -7,27 +7,28 @@ import { readableDate } from '../shared/helpers/readable-date.function';
 import { isInDocker } from '../shared/helpers/is-in-docker';
 
 @Injectable()
-export class PdfGeneratorService {
+export class PdfGeneratorService implements OnApplicationBootstrap, OnApplicationShutdown {
 
   private normalizeCssPath = `${__dirname}/assets/css/normalize.css`;
   private typographyCssPath = `${__dirname}/assets/css/typography.css`;
   private orderCssPath = `${__dirname}/assets/css/pdf-order.css`;
   private orderHtmlPath = `${__dirname}/assets/pdf-order.html`;
 
+  private browser: puppeteer.Browser;
+
+  async onApplicationBootstrap(): Promise<any> {
+    await this.launchBrowser();
+  }
+
+  async onApplicationShutdown(signal?: string): Promise<any> {
+    await this.closeBrowser();
+  }
+
   async generateOrderPdf(order: Order): Promise<Buffer> {
     const htmlFile = fs.readFileSync(this.orderHtmlPath, 'utf8');
     const html = handlebars.compile(htmlFile)(this.buildTemplateContextForOrder(order));
 
-    const launchOptions: puppeteer.LaunchOptions = {
-      headless: true,
-      args: ['--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox']
-    };
-    if (isInDocker()) {
-      launchOptions.executablePath = '/usr/bin/chromium-browser';
-    }
-
-    const browser = await puppeteer.launch(launchOptions);
-    const [page] = await browser.pages();
+    const page = await this.browser.newPage();
 
     await page.setContent(html);
     await page.addStyleTag({ path: this.typographyCssPath });
@@ -37,7 +38,7 @@ export class PdfGeneratorService {
 
     const pdf = await page.pdf({ format: 'A4', printBackground: true });
 
-    await browser.close();
+    await page.close();
 
     return pdf;
   }
@@ -68,6 +69,22 @@ export class PdfGeneratorService {
       discountPercent: order.discountPercent,
       discountValue: order.discountValue
     };
+  }
+
+  private async launchBrowser() {
+    const launchOptions: puppeteer.LaunchOptions = {
+      headless: true,
+      args: ['--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox', "--disable-gpu", "--single-process", "--no-zygote"]
+    };
+    if (isInDocker()) {
+      launchOptions.executablePath = '/usr/bin/chromium-browser';
+    }
+
+    this.browser = await puppeteer.launch(launchOptions);
+  }
+
+  private async closeBrowser() {
+    await this.browser.close();
   }
 }
 
