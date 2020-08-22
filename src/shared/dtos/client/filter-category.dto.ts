c3bd315e035b630e8c4f8c6163905bbb774ae228
Malikov Denis <ryki.admirala@gmail.com>
2020-08-22 17:16:07 +0300

    [KL-310] fixes in DTO

--- src/shared/dtos/client/filter-category.dto.ts
+++ src/shared/dtos/client/filter-category.dto.ts
@@ -1,12 +1,13 @@
 import { Category } from '../../../category/models/category.model';
 import { ClientMediaDto } from './media.dto';
-import { Expose } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 
 export class FilterCategoryDto implements Record<keyof Pick<Category, 'id' | 'name' | 'slug' | 'medias'>, any> {
   @Expose()
   id: number;
 
   @Expose()
+  @Type(() => ClientMediaDto)
   medias: ClientMediaDto[];
 
   @Expose()
