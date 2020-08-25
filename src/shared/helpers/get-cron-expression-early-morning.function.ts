4e649cb9c725cbe568169ccc3e5492edd130ac3b
Malikov Denis <ryki.admirala@gmail.com>
2020-08-25 20:22:04 +0300

    variable name optimization

--- src/shared/helpers/get-cron-expression-early-morning.function.ts
+++ src/shared/helpers/get-cron-expression-early-morning.function.ts
@@ -1,11 +1,12 @@
 const STARTING_HOUR = 4;
 const STARTING_MINUTE = 10;
 const MINUTES_IN_HOUR = 60;
+const DELAY_BETWEEN_JOBS_IN_MINS = 3;
 
 let jobsCount = 0;
 
 export function getCronExpressionEarlyMorning() {
-  const minute = (STARTING_MINUTE + (jobsCount * 3)) % MINUTES_IN_HOUR;
+  const minute = (STARTING_MINUTE + (jobsCount * DELAY_BETWEEN_JOBS_IN_MINS)) % MINUTES_IN_HOUR;
   const hour = STARTING_HOUR + Math.floor(minute / MINUTES_IN_HOUR);
 
   jobsCount++;
