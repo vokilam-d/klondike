940684cded048fe81db9f03d37f2697d33d45cae
Malikov Denis <ryki.admirala@gmail.com>
2020-08-26 00:41:44 +0300

    [KL-324] remove categories from "ClientProductListResponseDto"

--- src/shared/dtos/client/product-list-response.dto.ts
+++ src/shared/dtos/client/product-list-response.dto.ts
@@ -1,9 +1,7 @@
 import { ClientFilterDto } from './filter.dto';
 import { ResponseDto } from '../shared-dtos/response.dto';
 import { ClientProductListItemDto } from './product-list-item.dto';
-import { FilterCategoryDto } from './filter-category.dto';
 
 export class ClientProductListResponseDto extends ResponseDto<ClientProductListItemDto[]> {
   filters?: ClientFilterDto[];
-  categories?: FilterCategoryDto[];
 }
