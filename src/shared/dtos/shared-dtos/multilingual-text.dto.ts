03150e6ea6d4ffacb00099cc62f854b357f394ca
Malikov Denis <ryki.admirala@gmail.com>
2020-12-27 02:21:35 +0200

    [KL-438] add @Expose() to multilingual-text.dto.ts

--- src/shared/dtos/shared-dtos/multilingual-text.dto.ts
+++ src/shared/dtos/shared-dtos/multilingual-text.dto.ts
@@ -1,16 +1,20 @@
 import { IsOptional, IsString } from 'class-validator';
 import { TrimString } from '../../decorators/trim-string.decorator';
 import { MultilingualText } from '../../models/multilingual-text.model';
+import { Expose } from 'class-transformer';
 
 export class MultilingualTextDto implements MultilingualText {
+  @Expose()
   @IsString()
   @TrimString()
   ru: string;
 
+  @Expose()
   @IsString()
   @TrimString()
   uk: string;
 
+  @Expose()
   @IsOptional()
   @IsString()
   @TrimString()
