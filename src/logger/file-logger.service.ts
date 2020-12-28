f0c24d72d16bbf35072031273a1a17696ec291ca
Malikov Denis <ryki.admirala@gmail.com>
2020-12-28 21:49:42 +0200

    fix FileLogger

--- src/logger/file-logger.service.ts
+++ src/logger/file-logger.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger, Scope } from '@nestjs/common';
 import { createWriteStream, WriteStream } from 'fs';
 import { isProdEnv } from '../shared/helpers/is-prod-env.function';
 
-@Injectable({ scope: Scope.TRANSIENT })
+@Injectable({ scope: Scope.DEFAULT })
 export class FileLogger extends Logger {
 
   private writeStream: WriteStream;
@@ -13,9 +13,28 @@ export class FileLogger extends Logger {
   }
 
   log(message: any, context?: string) {
-    this.writeToFile(message);
-
+    this.writeToFile(message, 'log');
     super.log(message, context);
+  }
+
+  error(message: any, context?: string) {
+    this.writeToFile(message, 'error');
+    super.error(message, context);
+  }
+
+  warn(message: any, context?: string) {
+    this.writeToFile(message, 'warn');
+    super.warn(message, context);
+  }
+
+  debug(message: any, context?: string) {
+    this.writeToFile(message, 'debug');
+    super.debug(message, context);
+  }
+
+  verbose(message: any, context?: string) {
+    this.writeToFile(message, 'verbose');
+    super.verbose(message, context);
   }
 
   private createWriteStream() {
@@ -24,12 +43,12 @@ export class FileLogger extends Logger {
     this.writeStream = createWriteStream(`/var/log/log.txt`, { flags: 'a' });
   }
 
-  private writeToFile(message: string) {
+  private writeToFile(message: string, type: 'log' | 'error' | 'warn' | 'debug' | 'verbose') {
     if (!this.writeStream) { return; }
 
     const date = new Date();
     date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
 
-    this.writeStream.write(`${date.toISOString()} - [${this.context}] ${message} \n`);
+    this.writeStream.write(`${date.toISOString()} - [${type.toUpperCase()}] - [${this.context}] ${message} \n`);
   }
 }
