5adab0f205777d7d8cbb5754efb9e6db18849fa9
Malikov Denis <ryki.admirala@gmail.com>
2021-03-13 22:25:25 +0200

    add console.log, $patch

--- src/auth/strategies/user-jwt.strategy.ts
+++ src/auth/strategies/user-jwt.strategy.ts
@@ -13,6 +13,7 @@ export class UserJwtStrategy extends PassportStrategy(Strategy, authConstants.US
   constructor(private userService: UserService) {
     super({
       jwtFromRequest: (req: FastifyRequest) => {
+        console.log({cookies: req?.cookies});
         const jwtCookie = req?.cookies?.[authConstants.JWT_ADMIN_COOKIE_NAME];
 
         if (isProdEnv()) {
