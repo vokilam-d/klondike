dff83a6ac22649a850154353919cdff17937b14c
Malikov Denis <ryki.admirala@gmail.com>
2021-05-11 23:40:45 +0300

    [KL-525] add validation to MaintenanceInfoDto, $patch

--- src/shared/dtos/shared-dtos/maintenance-info.dto.ts
+++ src/shared/dtos/shared-dtos/maintenance-info.dto.ts
@@ -1,9 +1,13 @@
 import { Expose } from 'class-transformer';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class MaintenanceInfoDto {
   @Expose()
+  @IsBoolean()
   isMaintenanceInProgress: boolean;
 
   @Expose()
+  @IsOptional()
+  @IsString()
   maintenanceEndTime: string | null;
 }
