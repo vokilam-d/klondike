a1835fe779c5fc52c8a40953bc6c3d5a613b037c
Malikov Denis <ryki.admirala@gmail.com>
2021-05-25 19:00:22 +0300

    add checkForMaintenance to ShoppingFeedService, $patch

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -4,9 +4,10 @@ import { FeedController } from './feed.controller';
 import { ProductModule } from '../product/product.module';
 import { ProductReviewModule } from '../reviews/product-review/product-review.module';
 import { AttributeModule } from '../attribute/attribute.module';
+import { MaintenanceModule } from '../maintenance/maintenance.module';
 
 @Module({
-  imports: [ProductModule, ProductReviewModule, AttributeModule],
+  imports: [ProductModule, ProductReviewModule, AttributeModule, MaintenanceModule],
   providers: [ShoppingFeedService],
   controllers: [FeedController]
 })
